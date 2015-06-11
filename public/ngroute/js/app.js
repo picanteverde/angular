c20c9ebc7eb74b692995895b73fa4a274046cbd1
Alejandro Hernández <picanteverde@gmail.com>
2015-06-11 01:03:16 -0300

    initial commit

---
+++ public/ngroute/js/app.js
@@ -0,0 +1,40 @@
+angular.module("firstApp", ["ngRoute"])
+	.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+		$routeProvider
+			.when("/", {
+				template: "<h1>Welcome</h1>"
+			})
+			.when("/list", {
+				templateUrl: "view/list.html",
+				controller: "ListController as ctrl"
+			})
+			.when("/new", {
+				templateUrl: "view/new.html",
+				controller: "NewController as ctrl"
+			});
+		$locationProvider.html5Mode(true);
+	}])
+	.service("personService", function() {
+		this.persons = [{
+			"name": "Alejandro",
+			"city": "Cordoba",
+		}, {
+			"name": "Mauricio",
+			"city": "San Luis"
+		}];
+	})
+	.controller({
+		"NewController": ["personService", function(personService) {
+			var that = this;
+			that.addName = function(newP) {
+				if (newP.name && newP.city) {
+					personService.persons.push(angular.copy(newP));
+					newP.name = "";
+					newP.city = "";
+				}
+			}
+		}],
+		"ListController": ["personService", function(personService){
+			this.persons = personService.persons;
+		}]
+	});
